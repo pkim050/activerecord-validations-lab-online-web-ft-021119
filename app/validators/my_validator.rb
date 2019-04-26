f60a220e50a03801cafc71292df6ee1ddd98a2a1
Patrick Kim <pkim050@gmail.com>
2019-04-26 09:52:19 +0000

    Automatically backed up by Learn

--- app/validators/my_validator.rb
+++ app/validators/my_validator.rb
@@ -1,5 +1,6 @@
 class MyValidator < ActiveModel::Validator
   def validate(post)
+    binding.pry
     unless post.title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
       return false
     end
