e5fefa887c87cd9d28bdf52390f93e29b9609048
Patrick Kim <pkim050@gmail.com>
2019-04-26 09:54:00 +0000

    Automatically backed up by Learn

--- app/validators/my_validator.rb
+++ app/validators/my_validator.rb
@@ -1,6 +1,7 @@
 class MyValidator < ActiveModel::Validator
   def validate(post)
     binding.pry
+    return false if post.title == nil
     unless post.title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
       return false
     end
